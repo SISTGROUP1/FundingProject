5b07f594a297133c356ce789183bc51c21e1ae2b
ParkHyeokGyu <parkhk1994@gmail.com>
2024-03-29 16:42:58 +0900

    1111

---
+++ FundingProject/src/main/java/com/sist/web/FundingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundingProjectApplication.class, args);
+	}
+
+}
